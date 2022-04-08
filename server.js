9c0d9cde4fac0674e905967b453754a9d3d78e14
Cruel-Crab <mohammadhirani-cmpn@atharvacoe.ac.in>
2022-04-08 15:55:42 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/khabar-ghar-news-website'));
+app.use(express.static(__dirname + '/dist/project-pokedex'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +
     '/dist/project-pokedex/index.html'));
